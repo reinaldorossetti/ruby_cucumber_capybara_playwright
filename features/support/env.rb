e9053f5e7e4e408f4ab8553e4b00bad3e9165a9b
Reinaldo Rossetti <reinaldo.rossetti@paygo.com.br>
2021-11-09 21:20:13 -0300

    tratando entrada das variaveis

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,24 +3,33 @@ require 'capybara/cucumber'
 require 'json'
 require 'allure-cucumber'
 require 'capybara-playwright-driver'
-require 'rspec/retry'
 
+# tratando as variaveis de ambiente e definindo valores padroes.
+if ENV['HEADLESS'].nil? or ENV['HEADLESS'].downcase=='false'
+  headless_browser = false
+else 
+  headless_browser = true
+end
+
+if ENV['BROWSER'].nil?
+  (browser_name = "chromium")
+else
+  browser_name = ENV['BROWSER'].downcase
+end
 
 # setup
 Capybara.register_driver(:playwright) do |app|
-  $page = Capybara::Playwright::Driver.new(app, playwright_cli_executable_path: './node_modules/.bin/playwright', 
-                                                browser_type: (ENV['BROWSER']).to_sym, headless: to_boolean(ENV['HEADLESS']),
-                                                slowMo: 400, args: %w[--window-size=1280,1024 --no-sandbox])
+  driver = Capybara::Playwright::Driver.new(app, playwright_cli_executable_path: './node_modules/.bin/playwright',
+                                                browser_type: browser_name.to_sym, headless: headless_browser,
+                                                slowMo: 500, args: %w[--window-size=1280,1024 --no-sandbox])
 end
 
-Capybara.default_max_wait_time = 15
+Capybara.default_max_wait_time = 20
 Capybara.default_driver = :playwright #:selenium
 Capybara.save_path = 'tmp/capybara'
 Capybara.javascript_driver = :playwright #:selenium
 Capybara.app_host = 'https://www.vr.com.br/'
 
-puts "Browser: #{Capybara.inspect}"
-
-def to_boolean(str)
-  str.downcase == 'true'
-end
+puts "Browser=#{browser_name}"
+puts "Headless=#{headless_browser}"
+puts "Session=#{driver.config}"
